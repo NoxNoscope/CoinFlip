b579d4e37ab5e132c33c3d8cab8146cfdc866bf4
NoxNoscope <Lennart.Diego.kahn@gmail.com>
2020-10-31 19:15:27 +0100

    inisial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,32 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    srand(time(0));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString s = QString::number(rand() % 2);
+    ui->label->setText(s);
+
+    QString aa = ui->lineEdit->text();
+    ui->lineEdit->clear();
+    if(aa == s){
+       ui->lost_won->setText("You Won");
+
+    }else {
+        ui->lost_won->setText("You Lost");
+
+    }
+}
